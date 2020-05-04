3de06eacf77f8d4548502f1ece5fa749bbc05bc1
caglaralkiss <caglaralkiss@gmail.com>
2020-05-04 03:20:21 +0300

    feat: add id instance to Player
    * introduce serializing definitions to Player model

--- src/main/java/com/caglaralkiss/tictactoe/model/Player.java
+++ src/main/java/com/caglaralkiss/tictactoe/model/Player.java
@@ -2,30 +2,37 @@ package com.caglaralkiss.tictactoe.model;
 
 import com.caglaralkiss.tictactoe.constants.PlayerState;
 import com.caglaralkiss.tictactoe.constants.SquareState;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Player {
 
+    private String id;
     private String name;
+    @JsonProperty(value = "mark")
     private SquareState squareState;
     private Integer score;
+    @JsonIgnore
     private PlayerState playerState;
 
-    public Player(String name) {
+    public Player(String id, String name) {
+        this.id = id;
         this.name = name;
     }
 
-    public Player(String name, SquareState squareState, Integer score, PlayerState playerState) {
+    public Player(String id, String name, SquareState squareState, Integer score, PlayerState playerState) {
+        this.id = id;
         this.name = name;
         this.squareState = squareState;
         this.score = score;
         this.playerState = playerState;
     }
 
-    public void makeMove(int row, int column, Board board) throws Exception {
-        Square square = board.getSquares()[row][column];
+    public void makeMove(Move move, Board board) throws Exception {
+        Square square = board.getSquares()[move.getRow()][move.getColumn()];
 
         if (square.isEmpty()) {
-            board.markSquare(this.squareState, row, column);
+            board.markSquare(this.squareState, move.getRow(), move.getColumn());
         } else {
             throw new Exception("Mark is not empty!");
         }
@@ -61,5 +68,13 @@ public class Player {
 
     public void setPlayerState(PlayerState playerState) {
         this.playerState = playerState;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
