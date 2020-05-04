1c60a3a49591d11a4eeefe3eda5269edc01dadfd
caglaralkiss <caglaralkiss@gmail.com>
2020-05-04 03:39:52 +0300

    feat: add GameState enum
    * add Turn enum
    * add Move

---
+++ src/main/java/com/caglaralkiss/tictactoe/constants/GameState.java
@@ -0,0 +1,8 @@
+package com.caglaralkiss.tictactoe.constants;
+
+public enum GameState {
+    PLAYING,
+    X_WIN,
+    O_WIN,
+    DRAW
+}
---
+++ src/main/java/com/caglaralkiss/tictactoe/constants/Turn.java
@@ -0,0 +1,6 @@
+package com.caglaralkiss.tictactoe.constants;
+
+public enum Turn {
+    X_TURN,
+    O_TURN
+}
